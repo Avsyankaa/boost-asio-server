3f69ed960b949f4a802e8e8bf4e4e0ceca11397b
Avsyankaa <Avsyankaa@gmail.com>
2018-12-23 09:50:59 +0000

    done

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,5 +1,11 @@
-// Copyright 2018 Your Name <your_email>
+// Copyright 2018 Avsyankaa <Avsyankaa@gmail.com>
 
 #include <gtest/gtest.h>
+#include <server.hpp>
 
-#include <header.hpp>
+TEST(talk_to_client, Test2) {
+    auto talk_ptr = std::make_shared<talk_to_client>();
+    SUCCEED();
+}
+
+
